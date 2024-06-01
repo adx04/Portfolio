fcf34d61ce06c1ebdfff2e051956bf1b23ce6efe
Ayush Dey <122734846+adx04@users.noreply.github.com>
2024-06-01 21:52:07 +0530

    LFS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:aae3d518402a951e120af40bfecd9c1e65edf640236c46bde3f73b86652defb1
+size 219
