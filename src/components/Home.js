1cca5a18f01039e8c8ff533a63099f55ce9a604c
Ayush Dey <122734846+adx04@users.noreply.github.com>
2024-06-03 01:05:32 +0530

    LFS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,3 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const HomeContainer = styled.div`
-  position: relative;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: ${({ theme }) => theme.background};
-  transition: background 0.5s ease;
-`;
-
-const NameSection = styled.div`
-  text-align: center;
-  color: ${({ theme }) => theme.textColor};
-`;
-
-const Home = () => {
-  return (
-    <HomeContainer id="home-page">
-      <NameSection>
-        <h1>Coding Enthusiast</h1>
-&nbsp;
-        <p>Campus Ambassador @Coding Ninjas</p>
-        <p>B.Tech (Hons.) (CSE) with Spl. in Fullstack AI</p>
-        <p>University of Petroleum & Energy Studies (UPES), Dehradun</p>
-        <p>Burdwan, West Bengal, India</p>
-      </NameSection>
-    </HomeContainer>
-  );
-};
-
-export default Home;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8ca39ade3cde2219fd4fe8d6ac2f3134be1e6ace78121c4dfe62fff680ecf68
+size 861
